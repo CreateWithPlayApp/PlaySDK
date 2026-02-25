6b3d091d38b8458fece8dffe662de4c087c44440
Tom OMalley <9475035+tomalley104@users.noreply.github.com>
2026-02-25 13:50:07 -0500

    [release] update Package.swift for 0.13.0-beta

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         ),
         .binaryTarget(
           name: "PlaySDKBinary",
-          url: "https://github.com/CreateWithPlayApp/PlaySDK/releases/download/v0.12.1-beta/_PlaySDK.xcframework.zip",
-          checksum: "5bae7edf9949c441b794af1bc2dc2ca439d2a3d29d938eb7f2282be19655edce"
+          url: "https://github.com/CreateWithPlayApp/PlaySDK/releases/download/v0.13.0-beta/_PlaySDK.xcframework.zip",
+          checksum: "62209af2ff2cb7efe3d4e34b116828976476fa3b9ebd9ad7f58add201d2ecfd6"
         )
     ]
 )
